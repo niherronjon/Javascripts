73257532d7b91501aa2406772c585be9591ed58d
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-06-23 23:24:21 +0600

    Create JavaScript Data_Types_&_Variables.js

---
+++ Basics/JavaScript Data_Types_&_Variables.js
@@ -0,0 +1,5 @@
+let name = "Niher";       // String
+let age = 22;             // Number
+let isStudent = true;     // Boolean
+let empty = null;         // Null
+let notSet;               // Undefined
