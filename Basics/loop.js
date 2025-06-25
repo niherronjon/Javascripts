95550eebad881a54e411c37ce7b09c2fd60f1ed3
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-06-25 17:58:58 +0600

    Create loop.js

---
+++ Basics/loop.js
@@ -0,0 +1,20 @@
+//  Loops (for, while, do while) 
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// While loop
+let j = 1;
+while (j <= 5) {
+  console.log(j);
+  j++;
+}
+
+// Do-while loop
+let k = 1;
+do {
+  console.log(k);
+  k++;
+} while (k <= 5);
