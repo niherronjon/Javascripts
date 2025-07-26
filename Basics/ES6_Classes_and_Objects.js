2e9e80f079580c5794bde3a5ab1725f6638ab59b
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-07-26 11:00:17 +0600

    Create ES6_Classes_and_Objects.js

---
+++ Basics/ES6_Classes_and_Objects.js
@@ -0,0 +1,13 @@
+class Student {
+    constructor(name, roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    display() {
+        return `Name: ${this.name}, Roll: ${this.roll}`;
+    }
+}
+
+let stu1 = new Student("Niher", 101);
+console.log(stu1.display());
