cfd1959dd4e2c34dc0f14957c352c5ea898b641f
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2025-08-04 23:28:12 +0600

    Create Type Checking.js

---
+++ Basics/Type Checking.js
@@ -0,0 +1,5 @@
+console.log(typeof "Hello");    // "string"
+console.log(typeof 42);         // "number"
+console.log(typeof true);       // "boolean"
+console.log(typeof undefined);  // "undefined"
+console.log(typeof null);       // "object" (this is a known quirk)
